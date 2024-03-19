27646f97f67c9d26365f3b026fcd13b4b28741dc
InnaBoron <inkaboron@gmail.com>
2024-03-19 17:28:06 +0200

    Refactored dependencies

---
+++ utility_ltyj_110643_41xh.tsx
@@ -0,0 +1,6 @@
+const squareRoot = num => Math.sqrt(num);
+
+const sumAllNumbers = (...numbers) => numbers.reduce((sum, num) => sum + num, 0);
+const formatDate = date => new Date(date).toLocaleDateString();
+const mapObject = (obj, fn) => Object.fromEntries(Object.entries(obj).map(([key, value]) => [key, fn(value)]));
+const sumOfPrimes = limit => { const isPrime = num => { for (let i = 2; i < num; i++) if (num % i === 0) return false; return num > 1; }; return Array.from({ length: limit }, (_, index) => index).filter(isPrime).reduce((sum, prime) => sum + prime, 0); };
